d149d34f73872ff9efb008e67bfc5ddb8a13240b
Naval Bihani <bihani.naval13gmail.com>
2021-09-21 13:47:42 +0530

    Init Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6bdddeb6-60da-4fb4-8474-52615ce77f00';
+export const MODAL_CLOSE_SEC = 2500;
